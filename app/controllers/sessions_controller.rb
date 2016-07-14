a743f4916913a833dd4ab1dac79982471c4f0512
Diogo Munhos <diogomunhos@gmail.com>
2016-07-14 16:03:59 -0300

    facebook tests

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,23 +4,29 @@ class SessionsController < ApplicationController
 
 	end
 
-	def create
-    user = User.find_by_email(params[:email])
-    # If the user exists AND the password entered is correct.
-    if user && user.authenticate(params[:password])
-      # Save the user id inside the browser cookie. This is how we keep the user 
-      # logged in when they navigate around our website.
-      session[:user_id] = user.id
-      redirect_to '/'
-    else
-    # If user's login doesn't work, send them back to the login form.
-      redirect_to '/signin'
-    end
-  end
+	# def create
+	# 	user = User.find_by_email(params[:email])
+ #   		 # If the user exists AND the password entered is correct.
+	#     if user && user.authenticate(params[:password])
+	#       # Save the user id inside the browser cookie. This is how we keep the user 
+	#       # logged in when they navigate around our website.
+	#       session[:user_id] = user.id
+	#       redirect_to '/'
+	# 	  else
+	# 	    # If user's login doesn't work, send them back to the login form.
+	# 	    redirect_to '/signin'
+	# 	end
+	# end
 
-  def destroy
-    session[:user_id] = nil
-    redirect_to '/signin'
-  end
+	def create
+	    user = User.from_omniauth(env["omniauth.auth"])
+	    session[:user_id] = user.id
+	    redirect_to root_url
+	end
+
+	def destroy
+		session[:user_id] = nil
+		redirect_to '/signin'
+	end
 
 end
