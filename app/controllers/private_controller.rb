10b3033f01b7983eace6303df23e1bd5e45fba77
Diogo Munhos <diogomunhos@gmail.com>
2016-08-23 21:07:52 -0300

    Profile feature

--- app/controllers/private_controller.rb
+++ app/controllers/private_controller.rb
@@ -3,21 +3,7 @@ class PrivateController < ApplicationController
 	before_filter :authorize, :profile_authorize, :has_to_change_password
 
 	def index 
-		@currentUser = User.find(session[:user_id])
-		@profile_image_url = "../assets/images/wahiga/logo-default-profile.jpg"
-		if(@currentUser.documents.length > 0)
-			@currentUser.user_documents.each do |doc|
-				if(doc.document_type === "profile_image")
-					@profile_image_url = "../images/show_image/#{@currentUser[0].documents[0].id}"
-					break
-				end
-			end
-		else
-			social = session[:social]
-			if(social != nil)
-				@profile_image_url = social['image_url']
-			end
-		end
+		
 	end
 
 	def get_permissions_service
