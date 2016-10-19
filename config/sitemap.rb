7178660821b23b4739f9763b895889042cd11dc7
Diogo <diogomunhos.seedts@sulamerica.com.br>
2016-10-19 16:52:51 -0200

    sitemaps hotfix

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,11 +1,11 @@
 SitemapGenerator::Sitemap.default_host = "https://www.wahiga.com"
 
 SitemapGenerator::Sitemap.adapter = SitemapGenerator::S3Adapter.new(
-  fogprovider: 'AWS',
-  awsaccesskeyid: ENV['AWS_ACCESS_KEY_ID'],
-  awssecretaccesskey: ENV['AWS_SECRET_ACCESS_KEY'],
-  fogdirectory: ENV['S3_BUCKET'],
-  fogregion: ENV['AWS_REGION'])
+  fog_provider: 'AWS',
+  aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+  aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+  fog_directory: ENV['S3_BUCKET'],
+  fog_region: ENV['AWS_REGION'])
 
 
 
