269b95f2ef5ac40a654272a7b1a9bfc0f0eb11a0
Diogo <diogomunhos.seedts@sulamerica.com.br>
2016-12-07 11:53:07 -0200

    improve performance on image upload

--- config/routes.rb
+++ config/routes.rb
@@ -145,7 +145,7 @@ Rails.application.routes.draw do
 
   get '/private/articles/send_to_approval/:articleid' => "articles_secured#send_to_approval"
 
-  get '/private/articles/update_image_performance' => "articles_secured#update_image_performance"
+  get '/private/articles/update_image_performance/:offset/:limit' => "articles_secured#update_image_performance"
 
 
   #Users
