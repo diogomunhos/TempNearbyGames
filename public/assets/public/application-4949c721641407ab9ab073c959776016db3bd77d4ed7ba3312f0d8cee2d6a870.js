5ff0ec1cdc0e4c47be1a017978532b458988f068
Diogo <diogomunhos.seedts@sulamerica.com.br>
2016-12-13 16:43:29 -0200

    login javascript

--- public/assets/public/application-4949c721641407ab9ab073c959776016db3bd77d4ed7ba3312f0d8cee2d6a870.js
+++ public/assets/public/application-4949c721641407ab9ab073c959776016db3bd77d4ed7ba3312f0d8cee2d6a870.js
@@ -11566,6 +11566,14 @@ return jQuery;
 angular.module('public-module', [
 	'public-module.article-services',
 	'public-module.article-controller',
+	'public-module.platform-services',
+	'public-module.platform-controller',
+	'public-module.signin-controller',
+	'public-module.signin-services',
+	'public-module.signup-controller',
+	'public-module.signup-services',
+	'public-module.social-login-controller',
+	'public-module.social-services',
 	'public-module.scrolldown-directive'
 ])
 ;
@@ -11804,6 +11812,66 @@ jQuery(document).ready(function() {
 }).call(this);
 'use strict';
 
+angular.module('public-module.platform-controller', [])
+.controller('platform-all-articles-controller', ['platformServices', '$scope', '$timeout', '$window', function(platformServices, $scope, $timeout, $window) {
+	
+	$scope.totalOfRecords = 0;
+	$scope.totalOfPages = 0;
+	$scope.actualPage = 1;
+	$scope.articles = [];
+	$scope.loadingArticles = true;
+	$scope.platform = ((document.getElementById("platform")  != null) ? document.getElementById("platform").value : "");
+
+
+	$scope.getArticles = function(){
+		platformServices.getArticles($scope.actualPage, 10, $scope.platform).then(function (result) {
+			$scope.loadingArticles = false;
+	        for(var i=0; i < result.data.length; i++){
+	        	$scope.articles.push({id: result.data[i].id, title: result.data[i].title, preview: result.data[i].preview, url: result.data[i].url, image_url: result.data[i].image_url});
+	        }
+	    });
+	}
+
+	$scope.testScroll = function(){
+		if(!$scope.loadingArticles){
+			$scope.loadingArticles = true;
+			$scope.actualPage++;
+			$scope.getArticles();
+		}
+	}
+
+	$scope.mobilecheck = function() {
+	  var check = false;
+	  (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+	  console.log(check);
+	  return check;
+	};
+
+	$scope.getArticles();
+	$scope.showSideBar = ($scope.mobilecheck() === true) ? false : true;
+
+}]);
+'use strict'
+angular.module('public-module.platform-services', [])
+	.service('platformServices', function($q, $http) {
+		
+		this.getArticles = function (pageNumber, numberPerPage, platform) {
+            var deferred = $q.defer();
+            $http({
+                method: 'GET',
+                url: "/"+platform+"/get-articles-paged/"+numberPerPage+"/"+pageNumber+".json"
+            }).then(function successCallback(response){ 
+                deferred.resolve(response);
+            }, function errorCallback(response){
+                deferred.reject(response);
+            });
+                   
+            return deferred.promise;
+        };
+
+});
+'use strict';
+
 angular.module('public-module.scrolldown-directive', [])
 .directive('scrolldown', function ($window) {
     return {
@@ -11816,6 +11884,401 @@ angular.module('public-module.scrolldown-directive', [])
             });
         }
     };
+});
+'use strict';
+
+angular.module('public-module.signin-controller', [])
+.controller('signin-controller', ['signinServices', '$scope', '$window', function(signinServices, $scope, $window) {
+
+	$scope.signin = {
+		email: '',
+		password: ''
+	}
+	$scope.showErrorMessage = false;
+	$scope.message = "";
+	$scope.loading = false;
+	$scope.showLink = false;
+	$scope.showConfirmationMessage = false;
+	$scope.confirmationMessage = "";
+
+	$scope.lastSigninTry = {
+		email: '',
+		password: ''	
+	}
+
+
+	$scope.login = function(){
+		$scope.lastSigninTry.email = $scope.signin.email;
+		$scope.lastSigninTry.password = $scope.signin.password;
+		signinServices.login($scope.signin).then(function (result) {
+			$scope.loading = false;
+			if(result.data.login){
+				$window.location.href = '/';
+			}else{
+				$scope.showErrorMessage = true;
+				$scope.showLink = result.data.showLink;
+				$scope.message = result.data.errorMessage;
+			}
+	    });
+	}
+
+	$scope.tryLogin = function(){
+		$scope.showConfirmationMessage = false;
+		$scope.loading = true;
+		if($scope.validateEmail($scope.signin.email)){
+			if($scope.signin.password != null && $scope.signin.password != ""){
+				$scope.showErrorMessage = false;
+				$scope.message = "";
+				$scope.login();
+			}else{
+				$scope.loading = false;
+				$scope.showErrorMessage = true;
+				$scope.message = "Digite sua senha";
+			}
+		}else{
+			$scope.loading = false;
+			$scope.showErrorMessage = true;
+			$scope.message = "Formato de email inválido";
+		}
+	}
+
+	$scope.sendEmailConfirmationLink = function(){
+		console.log('test');
+		$scope.showErrorMessage = false;
+		$scope.showConfirmationMessage = true;
+		$scope.confirmationMessage = "O email de confirmação foi enviado para " + $scope.lastSigninTry.email + " verifique sua caixa de email.";
+		signinServices.sendConfirmationlink($scope.lastSigninTry).then(function (result) {});
+	}
+
+
+	$scope.validateEmail = function(email){
+		var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    	return re.test(email);
+	}
+
+}])
+;
+'use strict'
+angular.module('public-module.signin-services', [])
+	.service('signinServices', function($q, $http) {
+
+	this.login = function (request) {
+        var deferred = $q.defer();
+        $http({
+            method: 'POST',
+            url: '/login-service.json',
+            data: {"email": request.email, "password": request.password},
+            headers: {'Content-Type': 'application/json', 'X-CSRF-Token': $('meta[name=csrf-token]').attr('content')}
+        }).then(function successCallback(response){ 
+            deferred.resolve(response);
+        }, function errorCallback(response){
+            deferred.reject(response);
+        });
+               
+        return deferred.promise;
+    };
+
+    this.sendConfirmationlink = function(request){
+        var deferred = $q.defer();
+        $http({
+            method: "POST",
+            url: "/send-confirmation-email.json",
+            data: {"email": request.email},
+            headers: {'Content-Type': 'application/json', 'X-CSRF-Token': $('meta[name=csrf-token]').attr('content')}
+        }).then(function successCallback(response){
+            deferred.resolve(response);
+        }, function errorCallback(response){
+            deferred.resolve(response);
+        });
+
+        return deferred.promise;
+    }
+
+});
+'use strict';
+
+angular.module('public-module.signup-controller', [])
+.controller('signup-controller', ['signupServices', '$scope', '$window', function(signupServices, $scope, $window) {
+
+	$scope.signup = {
+		name:{	value: '', 
+			   	error: false,
+			   	errorMessage: ''},
+		lastName:{	value: '', 
+			   	error: false,
+			   	errorMessage: ''},
+		nickname:{	value: '', 
+			   	error: false,
+			   	errorMessage: ''},
+		birthdate:{	value: '', 
+			   	error: false,
+			   	errorMessage: ''},	   		   		   	
+		email:{	value: '', 
+			   	error: false,
+			   	errorMessage: ''},
+		password: {value: '', 
+			   	error: false,
+			   	errorMessage: ''},
+		passwordConfirmation: {value: '', 
+			   	error: false,
+			   	errorMessage: ''}	   	
+	};
+
+	$scope.showErrorMessage = false;
+	$scope.message = "";
+	$scope.loading = false;
+	$scope.showForm = true;
+	$scope.showSuccessMessage = false;
+
+
+	$scope.signupMethod = function(){
+		signupServices.signup($scope.signup).then(function (result) {
+			$scope.loading = false;
+			console.log(result);
+			if(!result.data.signup){
+				$scope.showErrorMessage = true;
+				$scope.message = result.data.errorMessage;
+			}else{
+				$scope.showForm = false;
+				$scope.showSuccessMessage = true;
+			}
+	    });
+	}
+
+	$scope.trySignup = function(){
+		$scope.loading = true;
+		var valid = true;
+
+		if($scope.signup.name.value === ''){
+			valid = false;
+			$scope.signup.name.error = true;
+			$scope.signup.name.errorMessage = "Campo obrigatório";
+		}else{
+			$scope.signup.name.error = false;
+			$scope.signup.name.errorMessage = "";
+		}
+
+		if($scope.signup.lastName.value === ''){
+			valid = false;
+			$scope.signup.lastName.error = true;
+			$scope.signup.lastName.errorMessage = "Campo obrigatório";
+		}else{
+			$scope.signup.lastName.error = false;
+			$scope.signup.lastName.errorMessage = "";
+		}
+
+		if($scope.signup.nickname.value === ''){
+			valid = false;
+			$scope.signup.nickname.error = true;
+			$scope.signup.nickname.errorMessage = "Campo obrigatório";
+		}else{
+			$scope.signup.nickname.error = false;
+			$scope.signup.nickname.errorMessage = "";
+		}
+
+		if($scope.signup.birthdate.value === ''){
+			valid = false;
+			$scope.signup.birthdate.error = true;
+			$scope.signup.birthdate.errorMessage = "Campo obrigatório";
+		}else if(!$scope.validateDate($scope.signup.birthdate.value)){
+			$scope.signup.birthdate.error = true;
+			$scope.signup.birthdate.errorMessage = "Formato de data inválido dd/mm/yyyy";
+		}else{
+			$scope.signup.birthdate.error = false;
+			$scope.signup.birthdate.errorMessage = "";
+		}
+
+		if($scope.signup.email.value === ''){
+			valid = false;
+			$scope.signup.email.error = true;
+			$scope.signup.email.errorMessage = "Campo obrigatório";
+		}else{
+			$scope.signup.email.error = false;
+			$scope.signup.email.errorMessage = "";
+		}
+
+		if($scope.signup.password.value === ''){
+			valid = false;
+			$scope.signup.password.error = true;
+			$scope.signup.password.errorMessage = "Campo obrigatório";
+		}else if($scope.signup.password.value.length < 8){
+			valid = false;
+			$scope.signup.password.error = true;
+			$scope.signup.password.errorMessage = "Password deve ter no minimo 8 digitos";
+		}else{
+			$scope.signup.password.error = false;
+			$scope.signup.password.errorMessage = "";
+		}
+
+		if($scope.signup.passwordConfirmation.value === ''){
+			valid = false;
+			$scope.signup.passwordConfirmation.error = true;
+			$scope.signup.passwordConfirmation.errorMessage = "Campo obrigatório";
+		}else if($scope.signup.passwordConfirmation.value != $scope.signup.password.value){
+			valid = false;
+			$scope.signup.passwordConfirmation.error = true;
+			$scope.signup.passwordConfirmation.errorMessage = "Passwords diferentes";
+		}else{
+			$scope.signup.passwordConfirmation.error = false;
+			$scope.signup.passwordConfirmation.errorMessage = "";
+		}
+
+
+		if($scope.validateEmail($scope.signup.email.value) ){
+			$scope.showErrorMessage = false;
+			$scope.message = "";
+			if(valid){
+				$scope.signupMethod();	
+			}
+		}else{
+			$scope.loading = false;
+			$scope.signup.email.error = true;
+			$scope.signup.email.errorMessage = "Formato de email inválido";
+		}
+	}
+
+
+	$scope.validateEmail = function(email){
+		var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    	return re.test(email);
+	}
+
+	$scope.validateDate = function(date){
+		var pattern =/^([0-9]{2})\/([0-9]{2})\/([0-9]{4})$/;
+		return pattern.test(date);
+	}
+
+}])
+;
+'use strict'
+angular.module('public-module.signup-services', [])
+	.service('signupServices', function($q, $http) {
+
+	this.signup = function (request) {
+        var deferred = $q.defer();
+        $http({
+            method: 'POST',
+            url: '/signup-service.json',
+            data: { "name": request.name.value,
+                    "last_name": request.lastName.value,
+                    "nickname": request.nickname.value,
+                    "email": request.email.value,
+                    "password": request.password.value,
+                    "password_confirmation": request.passwordConfirmation.value,
+                    "birthdate": request.birthdate.value},
+            headers: {'Content-Type': 'application/json', 'X-CSRF-Token': $('meta[name=csrf-token]').attr('content')}
+        }).then(function successCallback(response){ 
+            deferred.resolve(response);
+        }, function errorCallback(response){
+            deferred.reject(response);
+        });
+               
+        return deferred.promise;
+    };
+
+
+});
+'use strict';
+
+angular.module('public-module.social-login-controller', [])
+.controller('social-login-controller', ['socialServices', '$scope', '$window', function(socialServices, $scope, $window) {
+
+	$scope.socialSignin = {
+		email: '',
+		password: ''
+	}
+
+	$scope.socialSignup = {
+		nickname: { value: '',
+					error: false,
+					errorMessage:''},
+		email: { 	value: '',
+					error: false,
+					errorMessage:''},
+		password: { value: '',
+					error: false,
+					errorMessage:''},
+		passwordConfirmation: { value: '',
+								error: false,
+								errorMessage:''}
+	}
+
+	$scope.showLoginErrorMessage = false;
+	$scope.loginMessage = "";
+	$scope.loginLoading = false;
+	$scope.showSignupErrorMessage = false;
+	$scope.signupMessage = "";
+	$scope.signupLoading = false;
+
+
+	$scope.login = function(){
+		socialServices.login($scope.socialSignin).then(function (result) {
+			$scope.loginLoading = false;
+			if(result.data.login){
+				$window.location.href = '/';
+			}else{
+				$scope.showLoginErrorMessage = true;
+				$scope.loginMessage = result.data.errorMessage;
+			}
+	    });
+	}
+
+	$scope.tryLogin = function(){
+		$scope.loginLoading = true;
+		if($scope.validateEmail($scope.socialSignin.email)){
+			if($scope.socialSignin.password != null && $scope.socialSignin.password != ""){
+				$scope.showLoginErrorMessage = false;
+				$scope.loginMessage = "";
+				$scope.login();
+			}else{
+				$scope.loginLoading = false;
+				$scope.showLoginErrorMessage = true;
+				$scope.loginMessage = "Digite sua senha";
+			}
+		}else{
+			$scope.loginLoading = false;
+			$scope.showLoginErrorMessage = true;
+			$scope.loginMessage = "Formato de email inválido";
+		}
+	}
+
+	$scope.signup = function(){
+
+	}
+
+	$scope.trySignup = function(){
+		
+	}
+
+
+	$scope.validateEmail = function(email){
+		var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    	return re.test(email);
+	}
+
+}])
+;
+'use strict'
+angular.module('public-module.social-services', [])
+	.service('socialServices', function($q, $http) {
+
+	this.login = function (request) {
+        var deferred = $q.defer();
+        $http({
+            method: 'POST',
+            url: '/login-social-service.json',
+            data: {"email": request.email, "password": request.password},
+            headers: {'Content-Type': 'application/json', 'X-CSRF-Token': $('meta[name=csrf-token]').attr('content')}
+        }).then(function successCallback(response){ 
+            deferred.resolve(response);
+        }, function errorCallback(response){
+            deferred.reject(response);
+        });
+               
+        return deferred.promise;
+    };
+
+
 });
 // Can be changed
 var strike_autostart = (typeof strike_autostart === "undefined") ? true : strike_autostart;
@@ -12702,20 +13165,3 @@ function setNewVolume() {
   });
 
 }).call(this);
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
-
-
-
-
-;
