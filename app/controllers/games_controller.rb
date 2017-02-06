d9fa3b24677bc460d4645d6b8c5381854b76bf2c
Diogo Munhos <diogomunhos.seedts@sulamerica.com.br>
2017-02-06 11:49:49 -0200

    image compress

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,7 +3,7 @@ class GamesController < ApplicationController
 	def show
 		@game = Game.find_by_friendly_url(params[:game])
 		@article = Article.
-		@images = ArticleDocument.
+		
 	end
 
 	def news
